0d522ce17b6e1edac9783e5c859b2d3517fc0614
Paul Reimer <paul@p-rimes.net>
2013-05-17 13:09:06 -0700

    Added ofxRingBufferTexture class, with API: addFrame(const std::vector<float>& frame) from audio loop, and uploadToTexture()/getTextureReference() in draw loop

---
+++ src/ofxRingBufferTexture.cpp
@@ -0,0 +1,115 @@
+/*
+ * Copyright Paul Reimer, 2013
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ * or send a letter to
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ */
+
+#include "ofxRingBufferTexture.h"
+
+//--------------------------------------------------------------
+ofxRingBufferTexture::ofxRingBufferTexture()
+: currentFrameOffset(0)
+, framesProcessed(0)
+, spectrumSize(0)
+, numFrames(0)
+{}
+
+//--------------------------------------------------------------
+ofxRingBufferTexture::~ofxRingBufferTexture()
+{}
+
+//--------------------------------------------------------------
+void
+ofxRingBufferTexture::setup(const size_t _spectrumSize, const size_t _numFrames)
+{
+  spectrumSize = _spectrumSize;
+  numFrames = _numFrames;
+
+  ofFbo::Settings settings;
+  settings.width          = spectrumSize;
+  settings.height         = numFrames;
+	settings.useDepth       = false;				// whether to use depth buffer or not
+	settings.useStencil     = false;				// whether to use stencil buffer or not
+	settings.textureTarget  = GL_TEXTURE_RECTANGLE_ARB;
+//	settings.internalformat = GL_LUMINANCE32F_ARB;			// GL_RGBA, GL_RGBA16F_ARB, GL_RGBA32F_ARB, GL_LUMINANCE32F_ARB etc.
+  framebuffer.allocate(settings);
+//  framebuffer.allocate(spectrumSize, numFrames, GL_LUMINANCE);
+
+  texture.allocate(spectrumSize, numFrames, GL_LUMINANCE32F_ARB, true);
+  texture.setTextureMinMagFilter(GL_NEAREST, GL_NEAREST);
+}
+
+//--------------------------------------------------------------
+void
+ofxRingBufferTexture::addFrame(const std::vector<float>& frame)
+{
+  queuedFrames.push_back(frame);
+}
+
+//--------------------------------------------------------------
+void
+ofxRingBufferTexture::uploadToTexture()
+{
+  if (!queuedFrames.empty())
+  {
+    texture.bind();
+
+    size_t firstFrameOffset;
+    while (!queuedFrames.empty())
+    {
+      const std::vector<float>& processFrame = queuedFrames.front();
+      {
+        // write to texture stage 1
+        glTexSubImage2D(texture.getTextureData().textureTarget, 0,
+                        0, currentFrameOffset,
+                        processFrame.size(), 1,
+                        GL_LUMINANCE, GL_FLOAT,
+                        processFrame.data());
+      }
+      queuedFrames.pop_front();
+      currentFrameOffset++;
+      framesProcessed++;
+
+      if (currentFrameOffset >= numFrames)
+        currentFrameOffset = 0;
+
+      firstFrameOffset = currentFrameOffset + 1;
+
+      if (framesProcessed < numFrames)
+        firstFrameOffset = 0;
+
+      if (firstFrameOffset >= numFrames)
+        firstFrameOffset = 0;
+    }
+    texture.unbind();
+    
+    // write to fbo stage 2
+    // draw 2 rectangle sections from current:max (older) then from 0:current (newer)
+    framebuffer.begin();
+    {
+      float w = spectrumSize;
+      float totalH = MIN(framesProcessed, numFrames);
+      float olderH = totalH - firstFrameOffset;
+      
+      // older data section
+      texture.drawSubsection(0, 0, w, olderH,
+                             0, firstFrameOffset, w, olderH);
+      
+      // newer data section
+      texture.drawSubsection(0, olderH, w, firstFrameOffset,
+                             0, 0, w, firstFrameOffset);
+    }
+    framebuffer.end();
+  }
+}
+
+//--------------------------------------------------------------
+ofTexture&
+ofxRingBufferTexture::getTextureReference()
+{
+  return framebuffer.getTextureReference();
+}
---
+++ src/ofxRingBufferTexture.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright Paul Reimer, 2013
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ * or send a letter to
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ */
+
+#pragma once
+
+#include "ofTexture.h"
+#include "ofFbo.h"
+#include <deque>
+
+class ofxRingBufferTexture
+{
+public:
+  ofxRingBufferTexture();
+  ~ofxRingBufferTexture();
+  
+  void setup(const size_t _spectrumSize, const size_t _numFrames);
+  void uploadToTexture();
+  void addFrame(const std::vector<float>& frame);
+  
+  ofTexture& getTextureReference();
+
+  ofTexture texture;
+  ofFbo framebuffer;
+
+  float currentFrameOffset;
+  size_t framesProcessed;
+  std::deque<std::vector<float> > queuedFrames;
+  
+  size_t spectrumSize;
+  size_t numFrames;
+};
